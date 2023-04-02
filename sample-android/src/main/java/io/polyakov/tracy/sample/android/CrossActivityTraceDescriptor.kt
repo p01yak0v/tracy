4c69e693026a96759a5fb7dba71131a429405e17
p01yak0v <daniil.polyakov@gmail.com>
2023-04-02 15:55:11 +0300

    Replace startMatcher and stopMatcher for CrossActivityTraceDescriptor with delegates

--- sample-android/src/main/java/io/polyakov/tracy/sample/android/CrossActivityTraceDescriptor.kt
+++ sample-android/src/main/java/io/polyakov/tracy/sample/android/CrossActivityTraceDescriptor.kt
@@ -1,20 +1,14 @@
 package io.polyakov.tracy.sample.android
 
-import io.polyakov.tracy.android.checkpoint.ActivityCheckpoint
 import io.polyakov.tracy.android.checkpoint.ActivityCheckpoint.ActivityState
+import io.polyakov.tracy.android.matcher.activityState
 import io.polyakov.tracy.model.AttributeExtractor
 import io.polyakov.tracy.model.CheckpointMatcher
 import io.polyakov.tracy.model.TraceDescriptor
 
 class CrossActivityTraceDescriptor : TraceDescriptor {
-    override val startMatcher = CheckpointMatcher {
-        it is ActivityCheckpoint && it.activityClass == MainActivity::class && it.state == ActivityState.CREATED
-    }
-
-    override val stopMatcher = CheckpointMatcher {
-        it is ActivityCheckpoint && it.activityClass == NotReallyMainActivity::class && it.state == ActivityState.RESUMED
-    }
-
+    override val startMatcher by activityState<MainActivity>(ActivityState.CREATED)
+    override val stopMatcher by activityState<NotReallyMainActivity>(ActivityState.RESUMED)
     override val cancelMatcher = CheckpointMatcher { false }
 
     override val name: String
