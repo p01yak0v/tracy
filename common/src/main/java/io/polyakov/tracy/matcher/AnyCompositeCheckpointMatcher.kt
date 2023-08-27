5b4bbc0f5a5666718eb68564674907af3664a68b
p01yak0v <daniil.polyakov@gmail.com>
2023-08-27 19:13:28 +0300

    Add tests for AnyCompositeCheckpointMatcher

--- common/src/main/java/io/polyakov/tracy/matcher/AnyCompositeCheckpointMatcher.kt
+++ common/src/main/java/io/polyakov/tracy/matcher/AnyCompositeCheckpointMatcher.kt
@@ -6,6 +6,12 @@ class AnyCompositeCheckpointMatcher(
     private vararg val matchers: CheckpointMatcher
 ) : CheckpointMatcher {
 
+    init {
+        require(matchers.isNotEmpty()) {
+            "No matchers passed for AnyCompositeCheckpointMatcher. At least one matcher is required."
+        }
+    }
+
     override fun matches(checkpoint: Checkpoint): Boolean {
         return matchers.any { it.matches(checkpoint) }
     }
